58dc37d75b7f43b8ee77cc58ede819ae63827c73
deion-lefdahl <dl255133@umconnect.umt.edu>
2025-03-31 21:01:31 -0600

    .
    ,

---
+++ Week 9/sketch.js
@@ -0,0 +1,25 @@
+function setup ()
+{
+    createCanvas (500,600);
+}
+function draw ()
+{
+    background(220);
+    textSize(25)
+    text("My Self Portrait, Deion Lefdahl",10, 20)
+   
+    circle(150,160,170);
+    
+    point(100,110);
+    point(160,110);
+    strokeWeight(25);
+
+
+    
+
+    triangle(20, 45, 56, 30, 60, 75);
+    line(300,30,320,340);
+
+    rect(220,230,240,250);
+    
+}
